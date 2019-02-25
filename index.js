6b9395a5df5e7e495311eaa8f79801a9d59ad382
Estevan <estevan.rodriguez10@yahoo.com>
2019-02-25 14:41:20 -0800

    added post to register pageand hashed passwords

--- index.js
+++ index.js
@@ -12,6 +12,37 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
+server.post('/api/register',async(req,res)=>{
+    try{
+      const [id]= await db('users').insert(req.body);
+      const user = await db('users').where({ id }).first();
+      const hash = bcrypt.hashSync(user.password, 10); 
+      user.password = hash;
+
+      res.status(201).json(user);
+    }catch(error){
+      res.status(500).json(error);
+      }
+  });
+
+  server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    db.where({ username })
+      .first()
+      .then(user => {
+        // check that passwords match
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'Invalid Credentials' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
 
 
 const port = 3000;
